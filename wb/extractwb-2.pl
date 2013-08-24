93705a4cb2141af5fe8a2be815aef3d0ead0012f
Wolfgang <wolfgang.ztoeg@web.de>
2013-08-24 17:40:33 +0200

    comment in extractwb: WB_RBLevel vs RedBalance BlueBalance

--- wb/extractwb-2.pl
+++ wb/extractwb-2.pl
@@ -16,22 +16,35 @@ my @NEF = qx!ls *.nef!;
 chomp @NEF;
 
 foreach my $i (@NEF) {
-    my ($wb,$wbf,$wblev,$mod) =
-      qx!exiftool -s -S -WhiteBalance -WhiteBalanceFineTune -WB_RBLevels -Model $i!;
-      chomp  ($wb,$wbf,$wblev,$mod);
+    my ( $wb, $wbf, $wblev, $mod ) =
+qx!exiftool -s -S -WhiteBalance -WhiteBalanceFineTune -WB_RBLevels -Model $i!;
+    chomp( $wb, $wbf, $wblev, $mod );
 
     next if $wb =~ /Auto/;
     $wb =~ s/Cool WHT FL/CoolWhiteFluorescent/;
     $wb =~ s/Sunny/DirectSunlight/;
 
-    $Fakenr{ $wb } = $k if ($k) = $wb =~ /^(\d+)K$/;
+    $Fakenr{$wb} = $k if ($k) = $wb =~ /^(\d+)K$/;
 
-    
     say "$Fakenr{$wb} $wb $wbf $wblev $mod";
-    if ( $mod=~/D800E$/xms){
-	    $mod=~s/D800E/D800/;
-	    say "$Fakenr{$wb} $wb $wbf $wblev $mod";
+    if ( $mod =~ /D800/ ) {
+        if ( $mod =~ /D800E/ ) {
+            $mod =~ s/D800E/D800/;
+        }
+        else {
+            $mod =~ s/D800/D800E/;
+        }
+        say "$Fakenr{$wb} $wb $wbf $wblev $mod";
     }
 
-
 }
+
+
+__END__
+exiftool -s -WhiteBalance -WhiteBalanceFineTune -WB_RBLevels -RedBalance -BlueBalance
+WhiteBalance                    : 2700K
+WhiteBalanceFineTune            : 0 0
+WB_RBLevels                     : 1.1796875 2.48828125 1 1
+RedBalance                      : 1.179688
+BlueBalance                     : 2.488281
+
--- wb/ufraw-tables-2.pl
+++ wb/ufraw-tables-2.pl
@@ -3,9 +3,10 @@
 use v5.14;
 my $w;
 while (<>) {
-    my (undef,$wb,$wbf,undef,$v1,$v2,undef,undef,$mak,@mod) = split /\s+/, $_;
+    my ( undef, $wb, $wbf, undef, $v1, $v2, undef, undef, $mak, @mod ) =
+      split /\s+/, $_;
 
-    my $m=join " ",@mod;
+    my $m = join " ", @mod;
 
     if ( $wb =~ /^\d/ ) {
         $w = "\"$wb\"";
