cb4e925c2455959892c6d4874e2958350676c727
Wolfgang <wolfgang.ztoeg@web.de>
2013-09-21 12:44:44 +0200

    running synoindex on new files. fast. immediate visibility in dlna/photo6

--- synology/do-eaDir.sh
+++ synology/do-eaDir.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # wolfgang.ztoeg@web.de 20130915
+declare -A Findex
+
 mkdir="mkdir"
 convert="convert"
+ssh="ssh"
 
 while read f;do
 	e="${f%/*}/@eaDir/${f##*/}"
@@ -9,11 +12,18 @@ while read f;do
 	b="$e/SYNOPHOTO_THUMB_B.jpg"
 	m="$e/SYNOPHOTO_THUMB_M.jpg"
 	s="$e/SYNOPHOTO_THUMB_S.jpg"
-	[ -d "$e" ] || $mkdir -vp "$e"
+	if [ ! -d "$e" ];then
+	       $mkdir -vp "$e"
+	       Findex[$f]=1
+	fi
 	[ -f "$x" ] || $convert -resize 1280x1280 "$f" "$x"
 	[ -f "$b" ] || $convert -resize 640x640   "$x" "$b"
 	[ -f "$m" ] || $convert -resize 320x320   "$b" "$m"
 	[ -f "$s" ] || $convert -resize 120x120   "$m" "$s"
 done < <(find /diskstation/photo -path "*/@eaDir" -prune -o -type f -print)
 
-ssh admin@diskstation "synoindex -R photo"
+
+for f in "${!Findex[@]}";do
+	$ssh admin@diskstation "synoindex -a ${f/diskstation/volume1}"
+	echo -n .
+done
