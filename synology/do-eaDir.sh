12037ce18f9df622272fda997fddf45f903ea3cc
Wolfgang <wolfgang.ztoeg@web.de>
2013-09-22 20:26:37 +0200

    more info in output

--- synology/do-eaDir.sh
+++ synology/do-eaDir.sh
@@ -11,7 +11,7 @@ mkdir="mkdir"
 convert="convert"
 ssh="ssh"
 
-[ -d "/$syno_mount/photo" ] || { echo no mount ;exit 1; }
+[ -d "/$syno_mount/photo" ] || { echo no mount; exit 1; }
 
 while read f;do
 	e="${f%/*}/@eaDir/${f##*/}"
@@ -19,11 +19,13 @@ while read f;do
 	b="$e/SYNOPHOTO_THUMB_B.jpg"
 	m="$e/SYNOPHOTO_THUMB_M.jpg"
 	s="$e/SYNOPHOTO_THUMB_S.jpg"
-	[ -d "$e" ] || { $mkdir -vp "$e"; Findex[$f]=1; }
-	[ -f "$x" ] || $convert -resize 1280x1280 "$f" "$x"
-	[ -f "$b" ] || $convert -resize 640x640   "$x" "$b"
-	[ -f "$m" ] || $convert -resize 320x320   "$b" "$m"
-	[ -f "$s" ] || $convert -resize 120x120   "$m" "$s"
+	F=0
+	[ -d "$e" ] || { $mkdir -p "$e"; F=1; Findex[$f]=1; echo -n "$f"; }
+	[ -f "$x" ] || { $convert -resize 1280x1280 "$f" "$x"; echo -n " X"; }
+	[ -f "$b" ] || { $convert -resize 640x640   "$x" "$b"; echo -n " B"; }
+	[ -f "$m" ] || { $convert -resize 320x320   "$b" "$m"; echo -n " M"; }
+	[ -f "$s" ] || { $convert -resize 120x120   "$m" "$s"; echo -n " S"; }
+	[ $F -eq 1 ] && { echo "."; }
 done < <(find /$syno_mount/photo -path "*/@eaDir" -prune -o -type f -print)
 
 
