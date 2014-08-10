348a3b4c085a8b18346d56099ebcb14410d02fd0
Wolfgang <wolfgang.ztoeg@web.de>
2014-08-10 21:29:30 +0200

    exclusive access due to opencl, preload nef

--- synology/dt-cli.sh
+++ synology/dt-cli.sh
@@ -7,12 +7,22 @@ dt_cli="/opt/darktable/bin/darktable-cli"
 IFS="," read xmp out <<< $1
 
 t="/tmp/${out##*/}"
+nef=${xmp%.xmp}
 
 echo -n "$out "
 
 [ -f "$t" ] && rm -v "$t"
 mtime=$(/usr/bin/stat --format="%y" "$xmp")
-/usr/bin/time -a -o /tmp/dt-cli.time -f "%E %C" $dt_cli "${xmp%.xmp}" "$t" $(identify -format "--width %W --height %H" "$out") --core -d opencl 2>/dev/null|grep summary
+
+cat "$nef" > /dev/null
+(
+	flock -x 9 || exit 1
+	/usr/bin/time -a -o /tmp/dt-cli.time -f "%E %C" $dt_cli "$nef" "$t"\
+	       	$(identify -format "--width %W --height %H" "$out")\
+	       	--core -d opencl 2>/dev/null|grep summary
+
+) 9>/var/lock/dt-cli.lock
+
 /usr/bin/touch --date="$mtime" "$xmp"
 [ -f "$t" ] && mv  "$t" "$out"
 
